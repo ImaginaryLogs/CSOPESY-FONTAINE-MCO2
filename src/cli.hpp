9d8d4ce921d1feb8f0b400a1f6c9301876575ba2
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-10-29 19:35:11 +0800

    Create cli.hpp

---
+++ src/cli.hpp
@@ -0,0 +1,23 @@
+// cli.h
+#pragma once
+#include "config.hpp"
+#include "cpu_worker.hpp"
+#include "generator.hpp"
+#include "reporter.hpp"
+#include "scheduler.hpp"
+#include "screen.hpp"
+
+class CLI {
+public:
+  CLI();
+  int run(); // main loop; returns exit code
+private:
+  void handle_command(const std::string &cmd);
+  Config cfg_;
+  bool initialized_{false};
+  Scheduler *scheduler_{nullptr};
+  std::vector<std::unique_ptr<CPUWorker>> cpu_workers_;
+  ScreenManager screen_mgr_;
+  ProcessGenerator *generator_{nullptr};
+  Reporter *reporter_{nullptr};
+};
