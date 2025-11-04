d0d56f3aed11c7ded53e2cb634cd2fe322d23e53
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-11-04 15:27:55 +0800

    fix(cpu worker): update loop vis-a-vis process API

--- src/cpu_worker.cpp
+++ src/cpu_worker.cpp
@@ -1,4 +1,5 @@
 #include "../include/cpu_worker.hpp"
+#include "../include/process.hpp"
 #include "../include/scheduler.hpp"
 #include <iostream>
 
@@ -15,23 +16,21 @@ CPUWorker::CPUWorker(uint32_t id, Scheduler &sched) : id_(id), sched_(sched) {}
 void CPUWorker::start() {
   if (running_.load())
     return;
-  #if DEBUG_CPU_WORKER
+#if DEBUG_CPU_WORKER
   std::cout << "CPUWorker " << id_ << " starting.\n";
-  #endif
+#endif
 
   running_.store(true);
   thread_ = std::thread(&CPUWorker::loop, this);
-
 }
 
-void CPUWorker::stop() { 
-  #if DEBUG_CPU_WORKER
+void CPUWorker::stop() {
+#if DEBUG_CPU_WORKER
   std::cout << "CPUWorker " << id_ << " stopping.\n";
-  #endif
-  running_.store(false); 
+#endif
+  running_.store(false);
   sched_.stop_barrier_sync();
 }
-
 
 void CPUWorker::join() {
   if (thread_.joinable()) {
@@ -44,11 +43,11 @@ void CPUWorker::loop() {
 
     while (sched_.is_paused())
       std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    
+
     sched_.tick_barrier_sync();
-    
+
     auto process = sched_.dispatch_to_cpu(this->id_);
-    
+
     uint32_t consumed_ticks = 1; // max ticks possible in one execute_tick call
 
     if (!process) {
@@ -57,14 +56,16 @@ void CPUWorker::loop() {
       continue;
     }
 
-    bool finished = process->execute_tick(sched_.current_tick(), sched_.get_scheduler_tick_delay(), consumed_ticks);
-    
-    if (finished) {
+    ProcessState state = process->execute_tick(
+        sched_.current_tick(), sched_.get_scheduler_tick_delay(),
+        consumed_ticks);
+
+    if (state == ProcessState::FINISHED) {
       sched_.release_cpu(this->id_, process, true, false);
-    } 
+    }
 
     sched_.tick_barrier_sync();
     // Here, scheduler increases timer. Second tick barrier is essential
-    sched_.tick_barrier_sync(); 
+    sched_.tick_barrier_sync();
   }
 }
